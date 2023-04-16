7c97484df544614193f012f65177c02e47abe77e
Mark Nosal <mark.c.nosal@gmail.com>
2023-04-15 22:15:58 -0500

    finished lab

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -2,7 +2,27 @@ import React from "react";
 import { actors } from "../data";
 
 function Actors() {
-  return <div>{/*{code here}*/}</div>;
+  console.log(actors)
+  return (
+    <div>
+      <h1>Actors Page</h1>
+      {actors.map(actor => {
+        return (
+          <div key={actor.name}>
+            <h3>Name: {actor.name}</h3>
+            <h4>Movies:</h4>
+            <ul>
+              {actor.movies.map(movie => {
+                return (
+                  <li key={movie}>{movie}</li>
+                )
+              })}
+            </ul>
+          </div>
+        )
+      })}
+    </div>
+  )
 }
 
 export default Actors;
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,7 +2,28 @@ import React from "react";
 import { movies } from "../data";
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+    <div>
+      <h1>Movies Page</h1>
+      {movies.map(movie => {
+        return (
+          <div key={movie.title}>
+            <h3>{movie.title}</h3>
+            <h5>Time: {movie.time}</h5>
+            <h5>Genres:</h5>
+            <ul>
+              {movie.genres.map(genre => {
+                return (
+                  <li key={genre}>{genre}</li>
+                )
+              })}
+            </ul>
+
+          </div>
+        )
+      })}
+    </div>
+    )
 }
 
 export default Movies;
